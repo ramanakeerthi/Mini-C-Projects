f5aec5bc78cde2ed4c6da205d50c2229a5fc7e76
Ramana Keerthi <ramanak@umich.edu>
2016-11-25 12:55:25 -0800

    Added the first input check

---
+++ Todo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+
+check_if_file_exists();
+
+int main() {
+	std::cout << "Welcome to your to-do list! You have two options:\n";
+	std::string input;
+	std::cout << "Enter 'Check' if you'd like to check a date, or 'Write' if you'd like to write to a date: ";
+	std::cin >> input;
+	std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+	while(input != "write" && input != "check") {
+		std::cout << "Sorry! Your input didn't match one of the two options. If you'd like to continue, enter 'write' or 'check' or 'exit' to exit the program\n";
+		std::cin >> input;
+		if(input == "exit")
+			exit(0);
+	}
+}
+
+bool check_if_file_exists() {
+	ifstream ifs; 
+	ifs.
+}
